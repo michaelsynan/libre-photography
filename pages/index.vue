e5ee6a65cd5c7b26bf80ef6668dd54b1a767b92c
mikey <mforest@protonmail.com>
2023-11-04 12:25:41 -0400

    z index fixing

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <div class="h-screen relative">
     <MainHero class="fixed z-10" />
   </div>
-  <div id="imagegallerywrapper" class="sticky-container absolute z-100">
-    <ImageGallery id="imagegallery1" :heading="'Street'" class="sticky-gallery py-10 bg-stone-100 shadow-top z-100" />
+  <div id="imagegallerywrapper" class="sticky-container absolute">
+    <ImageGallery id="imagegallery1" :heading="'Street'" class="sticky-gallery py-10 bg-stone-100 shadow-top z-90" />
     <ImageGallery2 id="imagegallery2" :heading="'Nature'"
       class="sticky-gallery pt-10 pb-2 bg-stone-100 shadow-top z-100" />
     <div class="h-20 bg-stone-100"></div>
@@ -11,12 +11,22 @@
 </template>
 
 <style scoped>
+
+/*
 #imagegallery1,
 #imagegallery2,
 #imagegallerywrapper,
 #anotheritem {
   z-index: 1000 !important;
 }
+*/
+
+
+#imagegallerywrapper {
+  z-index: 1000 !important;
+}
+
+
 
 #anotheritem {
   z-index: 9;
